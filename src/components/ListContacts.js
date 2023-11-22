a353ed6955ad41b0f12bbec034f8df8ee3defbdc
Jude Abormegah <abordem@gmail.com>
2023-11-22 21:36:49 +0100

    Add state

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,6 +1,29 @@
-const ListContacts = ({ contacts }) => {
+const ListContacts = ({ contacts, onDeleteContact }) => {
   console.log(contacts);
-  return <ol className="contact_list"></ol>;
+  return (
+    <ol className="contact-list">
+      {contacts.map((contact) => (
+        <li key={contact.id} className="contact-list-item">
+          <div
+            className="contact-avatar"
+            style={{
+              backgroundImage: `url(${contact.avatarURL})`,
+            }}
+          ></div>
+          <div className="contact-details">
+            <p>{contact.name}</p>
+            <p>{contact.handle}</p>
+          </div>
+          <button
+            className="contact-remove"
+            onClick={() => onDeleteContact(contact)}
+          >
+            Remove
+          </button>
+        </li>
+      ))}
+    </ol>
+  );
 };
 
 export default ListContacts;
